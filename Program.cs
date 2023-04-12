f1242e42df4e493fb727ae48229a041b2887f9a6
murinosenshi <murinosenshi@gmail.com>
2023-04-12 18:13:07 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,9 +7,12 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
 using HtmlAgilityPack;
+using System.Xml;
+using System.Text;
 
 namespace scaner_sudov {
     class Program {
+        static String date_my = DateTime.UtcNow.ToString("dd.MM.yyyyy");
         Dictionary<byte, string> sp = new Dictionary<byte, string>()
         {
             {151, "Экономический суд Брестской области" },
@@ -47,14 +50,14 @@ namespace scaner_sudov {
         }
         static void tabl(IWebDriver drv, byte key) {
             Program p = new Program();
-            System.Threading.Thread.Sleep(2000);
+            Thread.Sleep(2000);
             captcha(drv);
             ReadOnlyCollection<IWebElement> els = drv.FindElements(By.ClassName("table-striped"));
             string[] sp_t = t_tabl(drv);
 
 
             for (byte ch = 0; ch < sp_t.Length; ch++) {
-                if (sp_t[ch]!=null) 
+                if (sp_t[ch] != null)
                     File.WriteAllText($"{p.sp[key]}{sp_t[ch]}.html", "<table>" + els[ch].GetAttribute("innerHTML") + "<table>");
             }
         }
@@ -95,7 +98,9 @@ namespace scaner_sudov {
                 string otvetchik = ss[7].InnerText.Replace("\n", "").Replace("  ", "").Replace("\r\r", " ").Replace("\r", "");
 
                 string itog = $"++ ДД|++ НН|++ И$|05 {now}|01 {nomer}|04 {o_chom}|02 {istets}|03 {otvetchik}|06 {name}|++ КК|++ ЯЯ|\n";
+                itog = itog.Replace(";", ",");
                 file.Write(itog);
+                //file.Write(itog);
             }
         }
         static void ADM(StreamWriter file, HtmlNode html, string name) {
@@ -107,15 +112,19 @@ namespace scaner_sudov {
                 string stia = ss[6].InnerText.Replace("\n", "").Replace("  ", "").Replace("\r\r", " ").Replace("\r", "");
 
                 string itog = $"++ ДД|++ НН|++ И$|05 {now}|01 {nomer}|03 {FIO}|04 {stia}|06 {name}|++ КК|++ ЯЯ|\n";
+                itog = itog.Replace(";", ",");
                 file.Write(itog);
+                //file.Write(itog);
             }
         }
 
         //основной метод
         static void Main(string[] args) {
+            string path = Directory.GetCurrentDirectory();
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
             Program p = new Program();
 
-            var driver = new ChromeDriver("d:\\programs\\chromedriver.exe");
+            var driver = new ChromeDriver();
             driver.Manage().Window.Position = new System.Drawing.Point(256, 0);
             driver.Navigate().GoToUrl("https://service.court.gov.by/ru/public/schedule");
 
@@ -123,7 +132,7 @@ namespace scaner_sudov {
                 captcha(driver);
                 oblast(i.Key, driver);
                 String today = DateTime.Now.Day.ToString();
-                System.Threading.Thread.Sleep(1000);
+                Thread.Sleep(1000);
 
                 ReadOnlyCollection<IWebElement> els = driver.FindElements(By.ClassName("day-schedule-active"));
                 byte butns = (byte)els.Count;
@@ -135,7 +144,7 @@ namespace scaner_sudov {
                         els[z].Click();
                         tabl(driver, i.Key);
                         break;
-                    } 
+                    }
                 }
             }
 
@@ -143,10 +152,10 @@ namespace scaner_sudov {
 
             //System.Environment.Exit(1);
 
-            string path = Directory.GetCurrentDirectory();
+
             string[] sf = Directory.GetFiles(path);
-            StreamWriter SUD_F = new StreamWriter(path + "\\1_SUD.txt", false);
-            StreamWriter ADM_F = new StreamWriter(path + "\\1_ADM.txt", false);
+            StreamWriter SUD_F = new StreamWriter(path + "\\1_SUD-"+date_my+".txt", false, Encoding.GetEncoding(1251));
+            StreamWriter ADM_F = new StreamWriter(path + "\\1_ADM-"+date_my+".txt", false, Encoding.GetEncoding(1251));
 
             foreach (var i in sf) {
                 if (!i.Contains("Экономический суд ")) continue;
@@ -157,13 +166,13 @@ namespace scaner_sudov {
                 doc.LoadHtml(html);
 
                 var table = doc.DocumentNode.SelectSingleNode("//tbody");
-                
+
                 string[] nm = i.Split(new char[] { '\\' });
                 string name = nm[nm.Length - 1];
 
-                if (i.Contains("SUD")) 
+                if (i.Contains("SUD"))
                     SUD(SUD_F, table, name.Substring(0, name.Length - 8));
-                else 
+                else
                     ADM(ADM_F, table, name.Substring(0, name.Length - 8));
 
                 File.Delete(i);
@@ -171,6 +180,9 @@ namespace scaner_sudov {
 
             SUD_F.Close();
             ADM_F.Close();
+
+            //File.WriteAllText(path + "\\1_SUD.txt", File.ReadAllText(path + "\\1_SUD.txt", Encoding.Default), Encoding.GetEncoding(1251));
+            //File.WriteAllText(path + "\\1_ADM.txt", File.ReadAllText(path + "\\1_ADM.txt", Encoding.Default), Encoding.GetEncoding(1251));
         }
     }
 }
